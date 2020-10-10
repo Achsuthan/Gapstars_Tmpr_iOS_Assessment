a00abfb9c06eb3d0056bf3463a1236fab3559832
Achsuthan <achsuthan@icloud.com>
2020-10-10 19:56:47 +0530

    Initial Commit

---
+++ Gapstars_Tmpr_iOS_AssessmentTests/Gapstars_Tmpr_iOS_AssessmentTests.swift
@@ -0,0 +1,34 @@
+//
+//  Gapstars_Tmpr_iOS_AssessmentTests.swift
+//  Gapstars_Tmpr_iOS_AssessmentTests
+//
+//  Created by Achsuthan Mahendran on 10/10/20.
+//  Copyright © 2020 Achsuthan Mahendran. All rights reserved.
+//
+
+import XCTest
+@testable import Gapstars_Tmpr_iOS_Assessment
+
+class Gapstars_Tmpr_iOS_AssessmentTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
